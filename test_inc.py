6ee68dce6925a528f56d30e0aa8755bf55930e76
byxmpy <104683754+byxmpy@users.noreply.github.com>
2022-09-13 15:15:33 +0800

    Add files via upload

---
+++ test_inc.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+def inc(x):
+    return x + 1
+
+
+class Test_inc:
+    @pytest.mark.inc
+    @pytest.mark.smoketest
+    @pytest.mark.functiontest
+    def test_business_zore(self):
+        assert inc(0) == 1
+
+    @pytest.mark.inc
+    @pytest.mark.functiontest
+    def test_business_positive(self):
+        assert inc(8) == 10
+
+    @pytest.mark.inc
+    @pytest.mark.functiontest
+    def test_business_negative(self):
+        assert inc(-1) == 0
+
+    @pytest.mark.inc
+    @pytest.mark.functiontest
+    def test_business_bignumber(self):
+        assert inc(999999998) == 1000000000
+
+    @pytest.mark.inc
+    @pytest.mark.illegalinputtest
+    @pytest.mark.xfail(raises=TypeError, reason="illegal input")
+    def test_business_illegalinput(self):
+        assert inc('a') == "a1"
